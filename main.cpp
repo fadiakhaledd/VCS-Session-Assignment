e20ae7e1f6d11b19bc071349b7fae24287f854aa
fadiakhaled <110059717+fadiakhaledd@users.noreply.github.com>
2023-10-26 03:28:48 +0300

    refactoring getAverage function

--- main.cpp
+++ main.cpp
@@ -19,15 +19,15 @@ double getAverage(std::vector<int> &numbers)
     }
     int sum = getSum(numbers);
     int vectorSize = numbers.size();
-    return sum / vectorSize;
+    return static_cast<double>(sum) / vectorSize;
 }
 
 int main()
 {
-    std::vector<int> inputNumbers = {1, 2, 3, 4, 5, 8, 7, 8, 9, 10};
+    std::vector<int> inputNumbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
     int numbersSum = getSum(inputNumbers);
-    int numbersAverage = getAverage(inputNumbers);
+    double numbersAverage = getAverage(inputNumbers);
 
     std::cout << "Sum of numbers: " << numbersSum << std::endl;
     std::cout << "Average of numbers: " << numbersAverage << std::endl;
